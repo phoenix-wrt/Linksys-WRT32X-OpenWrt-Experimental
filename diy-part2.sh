9d0304b3849ec8ddf42c8ad273bc34f57d44c7da
phoenix-wrt <109275742+phoenix-wrt@users.noreply.github.com>
2024-10-06 13:47:25 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,11 +13,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Add cron job to run update_hosts.sh every Monday at 4 AM if it doesn't already exist
-# Проверяем, существует ли файл, прежде чем пытаться использовать grep или изменять его
-if [ -f package/base-files/files/etc/crontabs/root ]; then
-    # Ваши существующие команды grep и модификации здесь
-    grep -q '0 5 * * 0 echo "Rebooting..." && reboot' package/base-files/files/etc/crontabs/root || echo '0 5 * * 0 echo "Rebooting..." && reboot' >> package/base-files/files/etc/crontabs/root
-else
-    echo "Предупреждение: файл crontab не найден. Пропускаем модификацию crontab."
-fi
+# Создаем директорию для crontab, если она не существует
+mkdir -p package/base-files/files/etc/crontabs
+
+# Создаем или обновляем файл crontab
+echo '0 4 * * 1 [ $(date +\%d) -le 7 ] && reboot' > package/base-files/files/etc/crontabs/root
