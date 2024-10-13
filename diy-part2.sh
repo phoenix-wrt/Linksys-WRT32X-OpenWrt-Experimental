357ddf51d6eeb3a8e96339f7a81013681e71a42d
phoenix-wrt <109275742+phoenix-wrt@users.noreply.github.com>
2024-10-13 21:25:19 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
+#
+# This file is part of the OpenWrt build process and is called after
+# the configuration is loaded.
+#
+# The script can be used to customize the build process further.
 
-# Add other customizations here
+# Enable and configure ccache
+echo "CONFIG_CCACHE=y" >> .config
+echo "CONFIG_CCACHE_DIR=$HOME/.ccache" >> .config
 
-# Create update_hosts.sh
+# Optimize ccache
+echo "export CCACHE_COMPRESS=1" >> $GITHUB_ENV
+echo "export CCACHE_COMPRESSLEVEL=5" >> $GITHUB_ENV
+echo "export CCACHE_MAXSIZE=2G" >> $GITHUB_ENV
+
+# Enable parallel build for packages
+echo "CONFIG_PKG_BUILD_JOBS=$BUILD_THREADS" >> .config
+echo "CONFIG_PKG_BUILD_PARALLEL=y" >> .config
+
+# Optimize for faster compilation
+echo "CONFIG_DEVEL=y" >> .config
+echo "CONFIG_BUILD_LOG=y" >> .config
+echo "CONFIG_CCACHE=y" >> .config
+echo "CONFIG_TOOLCHAINOPTS=y" >> .config
+echo "CONFIG_GCC_USE_VERSION_9=y" >> .config
+
+# Add custom packages (example)
+# echo "CONFIG_PACKAGE_vim=y" >> .config
+# echo "CONFIG_PACKAGE_nano=y" >> .config
+
+# Create update_hosts.sh script
 mkdir -p files/usr/bin
 cat << 'EOF' > files/usr/bin/update_hosts.sh
 #!/bin/sh
@@ -54,4 +81,33 @@ fi
 EOF
 
 chmod +x files/usr/bin/update_hosts.sh
-echo "update_hosts.sh script added to the firmware"
+
+# Add a cron job to update hosts file on the first Monday of each month at 4 AM
+mkdir -p files/etc/crontabs
+echo "0 4 1-7 * 1 [ $(date +\%d) -le 7 ] && /usr/bin/update_hosts.sh" >> files/etc/crontabs/root
+
+# Customize default IP address for LAN interface
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+# Modify default theme (optional)
+# sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
+
+# Add custom files or configurations
+# cp -r $GITHUB_WORKSPACE/custom_files/* files/
+
+# Apply patches
+# git apply $GITHUB_WORKSPACE/patches/*.patch
+
+# Customize hostname
+sed -i 's/OpenWrt/Linksys02023/g' package/base-files/files/bin/config_generate
+
+# Enable WiFi by default
+sed -i 's/option disabled 1/option disabled 0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Set timezone
+sed -i "s/timezone='UTC'/timezone='Europe\/Kiev'/g" package/base-files/files/bin/config_generate
+
+# Add other customizations below
+
+# Print disk usage for debugging
+df -h
